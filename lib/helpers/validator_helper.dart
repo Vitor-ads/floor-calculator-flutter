7b4b9fc9d7635710d7fcd4e7ea86c2e94e41351a
vitor <vitor.marcolin@e-deploy.com.br>
2021-04-19 22:49:18 -0300

    adicionado validador para os inputs

--- lib/helpers/validator_helper.dart
+++ lib/helpers/validator_helper.dart
@@ -1,5 +1,11 @@
+import 'package:floor_calculator/helpers/parse_helper.dart';
+
 class ValidatorHelper {
   static String isValidText(String text) {
-    return text.isEmpty ? 'Campo obrigatório' : null;
+    return text.isEmpty
+        ? 'Campo obrigatório'
+        : ParseHelper.toDouble(text) <= 0.0
+            ? 'valor invalido'
+            : null;
   }
 }
